ca56e4aae244aee0be67dccdbe3877f9b62b2358
Lucas <lucaspauwels@outlook.com>
2022-01-10 21:40:56 +0100

    fixes: text border color

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,19 @@ picker.setOptions({
     popup: "bottom",
 })
 picker.onChange = function (color) {
-    let editedHslString = color.hslString.slice(0, -6) + "25%)";
+    let hslStringArr = [...color.hslString.matchAll('hsl\\(([0-9]+(?:\\.[0-9]+)?), ?([0-9]+(?:\\.[0-9]+)?)%, ?([0-9]+(?:\\.[0-9]+)?)%\\)')]
+    let textBorderColor;
+    if(parseFloat(hslStringArr[0][3]) == 0) {
+        textBorderColor = "white";
+    } else if(parseFloat(hslStringArr[0][3]) == 100) {
+        textBorderColor == "black"
+    } else {
+        textBorderColor = color.hslString.replace(/[0-9]+(\.[0-9]+)?%\)/,'25%)');
+    }
     territory.style.color = color.hex;
     territory.style.borderColor = color.hex;
     territory.style.backgroundColor = color.hex.slice(0, -2) + "66";
-    console.info(editedHslString);
-    territory.style.textShadow = `2px 2px 0 ${editedHslString}, 2px -2px 0 ${editedHslString}, -2px 2px 0 ${editedHslString}, -2px -2px 0 ${editedHslString}, 2px 0px 0 ${editedHslString}, 0px 2px 0 ${editedHslString}, -2px 0px 0 ${editedHslString}, 0px -2px 0 ${editedHslString}, 0px 0px 3px rgba(255,0,0,0)`
+    territory.style.textShadow = `2px 2px 0 ${textBorderColor}, 2px -2px 0 ${textBorderColor}, -2px 2px 0 ${textBorderColor}, -2px -2px 0 ${textBorderColor}, 2px 0px 0 ${textBorderColor}, 0px 2px 0 ${textBorderColor}, -2px 0px 0 ${textBorderColor}, 0px -2px 0 ${textBorderColor}, 0px 0px 3px rgba(255,0,0,0)`
     hexInput.value = color.hex.slice(0, -2)
 
 }
